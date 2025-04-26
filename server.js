c782d91c7a289af2b65679bb75f36ce1e1ff9e42
Anupam0806 <153601938+anupampythoncodes@users.noreply.github.com>
2025-04-26 22:19:04 +0530

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(cors({
-  origin: 'https://passop07.netlify.app/',  // Allow your frontend's URL
+  origin: 'https://passop07.netlify.app',  // Allow your frontend's URL
   credentials: true,                // If you're sending cookies or authentication headers
 }));
 
